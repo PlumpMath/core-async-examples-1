995de1100fc53cb42390314a5f62c98d13c4cf04
John Madison <john.c.madison@gmail.com>
2013-10-12 21:45:33 -0500

    Filter out favicons

--- src/throttling-server.clj
+++ src/throttling-server.clj
@@ -7,6 +7,11 @@
 (defn strip [message]
   (clojure.string/replace (clojure.string/replace-first message #"/" "") #"%20" " "))
 
+;;determine if request is for favicon (can be ignored)
+(defn is-not-favicon? [message]
+  (nil? (re-matches #"favicon.ico" message)))
+
+;;channel of requests to handle
 (def queue (chan 1024))
 
 (defn handle [connection]
@@ -16,24 +21,27 @@
           request (.readLine inFromClient)
           message (when (not (nil? request))(strip (second (clojure.string/split request #" "))))
           result-chan (chan)]
-      (>! queue (list message result-chan))
-      (let [val (<! result-chan)
+      (when (is-not-favicon? message)
+        (>! queue (list message result-chan))
+        (let [val (<! result-chan)
             detailed-message (str message " - Served by go block #" val " - "(.toString (java.util.Date.)))]
         (.write outToClient (str "HTTP/1.1 200 OK\r\n" "Content-length: " (count detailed-message) "\r\n" "Content-type: text/html\r\n" "\r\n" detailed-message))
         (.flush outToClient)
         (.close inFromClient)
-        (.close outToClient)))))
+        (.close outToClient))))))
 
 (thread
   (let [socket (ServerSocket. 80)]
     (while true
+      (println "Waiting")
     (let [connection (.accept socket)]
       (handle connection)))))
 
 (thread
-  (doseq [i (range 1e3)]
+  (doseq [i (range 4)]
     (go
       (while true
+        (println "running ... " i)
         (let [[message result-chan] (<! queue)]
           (println (str "Go block " i " serving: " message ))
           (alts! [(timeout 5000)])   ;;fake a long process by parking go block
